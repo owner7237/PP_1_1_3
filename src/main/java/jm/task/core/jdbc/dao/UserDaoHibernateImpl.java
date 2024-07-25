5c1e3138607e58edd48a2b88a33713a53aa49899
owner7237 <132247769+owner7237@users.noreply.github.com>
2024-07-25 17:30:49 +0300

    правки

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,6 +5,7 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -33,79 +34,4 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
 
     }
-
-    /*public void createUsersTable() {
-        String sql = "create table users (id integer unique auto_increment primary key, name varchar(45), lastname varchar(45), age tinyint unsigned)";
-        try (var con = Util.getConnection(); var statement = con.createStatement()){
-            statement.execute(sql);
-        } catch (SQLException e) {
-            System.err.println("Ошибка создания таблицы: " + e.getMessage());
-        }
-    }
-
-    public void dropUsersTable() {
-        String sql = "drop table users";
-        try (Statement statement = Util.getConnection().createStatement()){
-            statement.execute(sql);
-        } catch (SQLException e) {
-            System.err.println("Ошибка удаления таблицы: " + e.getMessage());
-        }
-    }
-
-    public void saveUser(String name, String lastName, byte age) {
-        String sql = "insert into users (name, lastname, age) VALUES (?, ?, ?)";
-        try (var con = Util.getConnection(); var preparedStatement = con.prepareStatement(sql)) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void removeUserById(long id) {
-        String sql = "delete from users where id = ?";
-        try (var con = Util.getConnection(); var preparedStatement = con.prepareStatement(sql)) {
-            preparedStatement.setLong(1, id);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<User> getAllUsers() {
-        ArrayList<User> users = new ArrayList<>();
-        ResultSet results;
-
-        String sql = "SELECT * FROM users";
-        try (var con = Util.getConnection(); var preparedStatement = con.prepareStatement(sql)) {
-            results = preparedStatement.executeQuery();
-
-
-            while (results.next()) {
-                long id = results.getLong(1);
-                String name = results.getString(2);
-                String lastname = results.getString(3);
-                byte age = results.getByte(4);
-                User user = new User(name, lastname, age);
-                user.setId(id);
-                users.add(user);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return users;
-    }
-
-    public void cleanUsersTable() {
-        String sql = "delete from users";
-        try (var con = Util.getConnection(); var statement = con.createStatement()){
-            statement.execute(sql);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
 }
